c2fbc16bdc6d85983d7a6e73153e2e76b69a3dec
Kelsen Tavares <tiadsrussas@gmail.com>
2024-09-25 09:50:10 -0300

    Adicionando BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$NNoNvDOkfWRCqedZHd8rO.2/7.tGrxaH1hP87ie5B46We/jTnJMVq');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$NNoNvDOkfWRCqedZHd8rO.2/7.tGrxaH1hP87ie5B46We/jTnJMVq');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
