4e6c0cf44fd4a49fc6b135419432e5126fb58594
Kelsen Tavares <tiadsrussas@gmail.com>
2024-09-25 14:50:51 -0300

    Implementando checklist do Spring Security

---
+++ src/main/java/com/devsuperior/demo/projections/UserDatailsProjection.java
@@ -0,0 +1,10 @@
+package com.devsuperior.demo.projections;
+
+public interface UserDatailsProjection {
+	
+	String getUserName();
+	String getPassword();
+	Long getRoleId();
+	String getAuthority();
+
+}
