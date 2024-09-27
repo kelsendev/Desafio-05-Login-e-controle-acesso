b92da0f23c9ce7ba865d96c9783ded9e6d38dfb9
Kelsen Tavares <tiadsrussas@gmail.com>
2024-09-27 09:35:53 -0300

    Consulta ProductMinDTO

--- src/main/java/com/devsuperior/demo/controllers/ProductController.java
+++ src/main/java/com/devsuperior/demo/controllers/ProductController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.demo.dto.ProductDTO;
+import com.devsuperior.demo.dto.ProductMinDTO;
 import com.devsuperior.demo.services.ProductService;
 
 
@@ -37,10 +38,10 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(
+    public ResponseEntity<Page<ProductMinDTO>> findAll(
             @RequestParam(name = "name", defaultValue = "") String name,
             Pageable pageable) {
-        Page<ProductDTO> dto = service.findAll(name, pageable);
+        Page<ProductMinDTO> dto = service.findAll(name, pageable);
         return ResponseEntity.ok(dto);
     }
 
--- src/main/java/com/devsuperior/demo/services/ProductService.java
+++ src/main/java/com/devsuperior/demo/services/ProductService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.demo.dto.ProductDTO;
+import com.devsuperior.demo.dto.ProductMinDTO;
 import com.devsuperior.demo.entities.Product;
 import com.devsuperior.demo.repositories.ProductRepository;
 import com.devsuperior.demo.services.exceptions.DatabaseException;
@@ -30,9 +31,9 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAll(String name, Pageable pageable) {
+    public Page<ProductMinDTO> findAll(String name, Pageable pageable) {
         Page<Product> result = repository.searchByName(name, pageable);
-        return result.map(x -> new ProductDTO(x));
+        return result.map(x -> new ProductMinDTO(x));
     }
 
     @Transactional
